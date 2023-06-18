70b9fb66dd8face87bcf00e5b099cd39aa42632a
martynafoksinska <martyna.a.foksinska@gmail.com>
2023-06-18 21:59:34 +0200

    Elements update

--- src/main/java/Pages/GooglePage.java
+++ src/main/java/Pages/GooglePage.java
@@ -3,6 +3,8 @@ package Pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.Keys;
+
 
 public class GooglePage {
 
@@ -19,6 +21,7 @@ public class GooglePage {
         searchInput.clear();
         searchInput.click();
         searchInput.sendKeys("just join it");
+        searchInput.sendKeys(Keys.RETURN);
         WebElement searchButton = driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[1]"));
         searchButton.click();
     }
